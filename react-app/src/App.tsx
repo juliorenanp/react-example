77b07976d279ae4b58b861bc587a468e2a73aff8
Julio Pinheiro <juliorenan@live.com>
2022-06-21 22:26:23 -0300

    example os properties

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,13 @@
 import Onepiece from "./components/Onepiece"
 
 function App() {
- return ( <Onepiece/>)
+ return ( 
+    <div>
+        <p><Onepiece text="Zoro"/></p> 
+        <p><Onepiece text="Luffy"/></p> 
+        <p><Onepiece text="Nami"/></p> 
+   </div>
+ )
 }
 
 export default App
--- react-app/src/components/Onepiece.tsx
+++ react-app/src/components/Onepiece.tsx
@@ -1,5 +1,9 @@
-function Onepiece() {
-    return <h1>One Piece</h1>
+type OneProps ={
+    text: string;
+}
+
+export function Onepiece(props: OneProps) {
+    return <h1>One Piece {props.text}</h1>
    }
    
    export default Onepiece
