1d50749b7ef55a5b44aee19bf3c6d874c6d8af4c
Julio Pinheiro <juliorenan@live.com>
2022-06-21 22:38:23 -0300

    state example

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,13 +1,31 @@
+import { useState } from "react"
 import Onepiece from "./components/Onepiece"
 
 function App() {
- return ( 
-    <div>
-        <p><Onepiece text="Zoro"/></p> 
+    const [crew, setMember] = useState<string[]>([
+        'Zoro',
+        'Luffy',
+        'Nami'
+    ])
+
+    function createMember(){
+        setMember([...crew,'test'])
+    }
+
+
+    return (
+        <div>
+            {crew.map(member => {
+                return <Onepiece text={member} />
+            })}
+
+            {/* <p><Onepiece text="Zoro"/></p> 
         <p><Onepiece text="Luffy"/></p> 
-        <p><Onepiece text="Nami"/></p> 
-   </div>
- )
+        <p><Onepiece text="Nami"/></p>  */}
+
+            <button onClick={createMember}>Add caracter</button>
+        </div>
+    )
 }
 
 export default App
